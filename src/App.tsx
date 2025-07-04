b808fb8bc67c7c68e8f2b4850f18ea101770cc2b
Vijay Guttula <vijayguttula001@gmail.com>
2025-07-04 13:25:38 +0530

    Separated pages Home and Mint

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
-import React, { useState } from "react";
-import "./App.css";
-import { WalletProvider, useWallet } from "./context/WalletContext";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { WalletProvider } from "./context/WalletContext";
 import Navbar from "./components/Navbar";
-import NFTForm from "./components/NFTForm";
-import PinataUpload from "./utils/PinataUpload";
-
-const Content: React.FC = () => {
-  const { account } = useWallet();
-  const [ipfsHash, setIpfsHash] = useState<string>("");
-
-  return (
-    <main className="flex flex-col items-center justify-center p-4 space-y-6">
-      {!account ? (
-        <div className="text-lg text-gray-600">
-          Please connect your wallet above to mint an NFT.
-        </div>
-      ) : (
-        <div className="w-full max-w-md space-y-4">
-          <div className="text-center">
-            Connected Account: <b>{account}</b>
-          </div>
-
-          {/* Upload file to Pinata and get IPFS hash */}
-          <PinataUpload onUpload={(hash) => setIpfsHash(hash)} />
-
-          {/* Create NFT with the uploaded IPFS URL */}
-          <NFTForm
-            account={account}
-            onComplete={(assetId) =>
-              alert(`✅ NFT Created successfully! Asset ID: ${assetId}`)
-            }
-            ipfsUrl={ipfsHash ? `https://ipfs.io/ipfs/${ipfsHash}` : ""}
-          />
-        </div>
-      )}
-    </main>
-  );
-};
+import MintPage from "./pages/Mint";
+import HomePage from "./pages/Home";
 
 const App: React.FC = () => {
   return (
     <WalletProvider>
-      <div className="min-h-screen flex flex-col bg-gray-100">
-        <Navbar />
-        <Content />
-      </div>
+      <Router>
+        <div className="min-h-screen flex flex-col bg-gray-100">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/mint" element={<MintPage />} />
+          </Routes>
+        </div>
+      </Router>
     </WalletProvider>
   );
 };
